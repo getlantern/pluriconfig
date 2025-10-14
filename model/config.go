5472d0531f596ab32dc695ddc7167bf1610b1ac3
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-10-14 10:52:49 -0300

    feat: adding models used across the repository

---
+++ model/config.go
@@ -0,0 +1,41 @@
+// Package model holds the data models used across the application.
+package model
+
+// Config is a generic configuration struct that holds a type identifier and options of any type.
+type Config[T any] struct {
+	Type    Provider
+	Options T
+}
+
+type AnyConfig = Config[any]
+
+type Provider string
+
+const (
+	ProviderSingBox Provider = "singbox"
+	ProviderURL     Provider = "url"
+)
+
+// V2RTransportOpts holds options for V2Ray transport configuration.
+type V2RTransportOpts struct {
+	Type        string
+	Host        string
+	Path        string
+	Method      string
+	ServiceName string
+}
+
+// VMESSConfig holds the configuration for a VMESS protocol.
+type VMESSConfig struct {
+	Addr     string `json:"add"`
+	Port     uint16 `json:"port"`
+	Aid      int    `json:"aid"`
+	ALPN     string `json:"alpn"`
+	Host     string `json:"host"`
+	ID       string `json:"id"`
+	Net      string `json:"net"`
+	Path     string `json:"path"`
+	Security string `json:"scy"`
+	Sni      string `json:"sni"`
+	TLS      string `json:"tls"`
+}
