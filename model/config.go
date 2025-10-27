90a0abe1e049dae3b86088034ceab60663d1249b
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-10-27 11:55:19 -0300

    feat: adding headers as part of expected v2ray config options

--- model/config.go
+++ model/config.go
@@ -26,6 +26,7 @@ type V2RTransportOpts struct {
 	Path        string
 	Method      string
 	ServiceName string
+	Headers     map[string][]string
 }
 
 // VMESSConfig holds the configuration for a VMESS protocol.
