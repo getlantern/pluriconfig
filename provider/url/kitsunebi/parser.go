cd6b9cec66c2d28f3965ef0e544cc583298ae7fb
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-11-13 14:03:06 -0300

    fix: adding @garmr-ulfr kitsunebi suggestions

--- provider/url/kitsunebi/parser.go
+++ provider/url/kitsunebi/parser.go
@@ -1,4 +1,7 @@
 // Package kitsunebi parses the VMess data with the kitsunebi format into a common model.VMess object
+// Example:
+// vmess://YXV0bzo1YWY1ZDBlYy02ZWEwLTNjNDMtOTNkYi1jYTMwMDg1MDNiZGJAMTgzLjIzMi41Ni4xNjE6MTIwMg
+// ?remarks=*%F0%9F%87%AF%F0%9F%87%B5JP%20-355%20TG@moon365free&obfsParam=%7B%22Host%22:%22183.232.56.161%22%7D&path=/v2ray&obfs=websocket&alterId=0
 package kitsunebi
 
 import (
@@ -48,7 +51,7 @@ func (p parser) Parse(ctx context.Context, data []byte) (*model.AnyConfig, error
 
 		arr21 := strings.Split(arr1[0], ":")
 		arr22 := strings.Split(arr1[1], ":")
-		if len(arr21) != 2 || len(arr22) != 2 {
+		if len(arr21) != 2 && len(arr22) != 2 {
 			return nil, fmt.Errorf("invalid kitsunebi format")
 		}
 
@@ -67,32 +70,17 @@ func (p parser) Parse(ctx context.Context, data []byte) (*model.AnyConfig, error
 			continue
 		}
 
-		if queryParams.Has("remarks") {
-			data.Name = queryParams.Get("remarks")
-		}
+		data.Name = queryParams.Get("remarks")
+		data.AlterID = queryParams.Get("alterId")
+		data.Path = queryParams.Get("path")
+		data.Security = "tls"
 
-		if queryParams.Has("alterId") {
-			data.AlterID = queryParams.Get("alterId")
-		}
-
-		if queryParams.Has("path") {
-			data.Path = queryParams.Get("path")
-		}
-
-		if queryParams.Has("tls") {
-			data.Security = "tls"
-		}
-
-		if queryParams.Has("allowInsecure") {
-			insecure := queryParams.Get("allowInsecure")
-			if insecure == "1" || insecure == "true" {
-				data.AllowInsecure = true
-			}
+		if insecure := queryParams.Get("allowInsecure"); insecure != "" {
+			data.AllowInsecure = insecure == "1" || insecure == "true"
 		}
 
 		if queryParams.Has("obfs") {
 			val := queryParams.Get("obfs")
-
 			data.Type = strings.ReplaceAll(strings.ReplaceAll(val, "websocket", "ws"), "none", "tcp")
 			if data.Type == "ws" {
 				for _, v := range queryParams["obfsParam"] {
